c07c8360ee45287da027afbace19a39727a8e0ec
RogerDodger <roger.dodger.thor@gmail.com>
2012-07-14 20:26:14 +0930

    favicon

--- index.php
+++ index.php
@@ -80,7 +80,6 @@ dirname(htmlentities($_SERVER["SCRIPT_NAME"]))."/lib/timestrings.php?list=all";
 }
 
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -88,13 +87,18 @@ dirname(htmlentities($_SERVER["SCRIPT_NAME"]))."/lib/timestrings.php?list=all";
 <head>
 
 <title>tz-info</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="shortcut icon" href="favicon.ico" />
+
 <link href="<?php echo $path ?>/css/main.css" rel="stylesheet" type="text/css" />
 <link href="<?php echo $path ?>/css/ui-lightness/jquery-ui-1.8.21.custom.css" rel="stylesheet" type="text/css" />
+
 <script src="<?php echo $path ?>/js/jquery-1.7.2.min.js" type="text/javascript"></script>
 <script src="<?php echo $path ?>/js/jquery-ui-1.8.21.custom.min.js" type="text/javascript"></script>
 <script src="<?php echo $path ?>/js/site.js" type="text/javascript"></script>
 
 </head>
+
 
 <body>
 
